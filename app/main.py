e2d3a91a86b33b5d0d9945b8ba0eff6cc73cab89
mr-smile003 <psmit95749@gmail.com>
2024-11-17 19:33:49 +0530

    xapi added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Security, Header
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
 from pydantic import BaseModel, Field
 import requests
 from io import BytesIO
@@ -9,13 +11,23 @@ import numpy as np
 # FastAPI app instance
 app = FastAPI()
 
+API_KEY_NAME = "x-api-key"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if not api_key_header or api_key_header != 'GpTh18VYNq9F9Hsvrbpa7XOK92qKFU6C':
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Invalid or missing API Key"
+        )
+    return api_key_header
+
 # Request model for input data
 class ImageURLs(BaseModel):
     image_url: str
 
 @app.post("/face_encodings/")
-async def get_encodings(request: ImageURLs):
-    # Get encodings for the single image URL
+async def get_encodings(request: ImageURLs, api_key: str = Security(get_api_key)):
     encodings = face_encodings(request.image_url)
     return {"encodings": encodings}
 
@@ -32,15 +44,13 @@ class FaceMatchRequest(BaseModel):
     tolerance: Optional[float] = Field(default=0.6, ge=0.0)
 
 @app.post("/face_match/")
-async def match_faces(request: FaceMatchRequest):
+async def match_faces(request: FaceMatchRequest, api_key: str = Security(get_api_key)):
     try:
-        # Convert all target encodings into numpy arrays
         targeted_encodings = [
             {"personId": target.personId, "encodings": np.array([float(e) for e in target.encodings])}
             for target in request.targetedEncodings
         ]
         
-        # Convert and flatten the incoming encoding
         encoding_np = np.array([float(e) for e in request.encoding]).flatten()
 
         matching_person_id = find_matching_person(
